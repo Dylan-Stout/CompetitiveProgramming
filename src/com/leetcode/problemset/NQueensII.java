13e60d8820606c6b35baa08af4558cb404fcef59
Dylan Stout <dylan.d.stout@gmail.com>
2018-07-29 16:46:36 -0600

    Nqueens backtrack algorightm loop counter

--- src/com/leetcode/problemset/NQueensII.java
+++ src/com/leetcode/problemset/NQueensII.java
@@ -33,7 +33,7 @@ import java.util.HashSet;
  */
 public class NQueensII {
 
-    public static final int TOTAL_SOLUTIONS = 0; 
+    public static int TOTAL_SOLUTIONS = 0; 
     public static HashSet<Integer> attackedColumns = new HashSet<Integer>(); 
     public static HashSet<Integer> attackedRows = new HashSet<Integer>(); 
 	public static boolean[][] board = null; 
@@ -42,6 +42,8 @@ public class NQueensII {
      * First call made in recursive backtracking algorithm. Sets up an NxN 2D boolean 
      * array for keeping track of queen positions. Makes call to recursive backtrack algorithm
      * to find queen combinations through brute force permutations. 
+     * 
+     * The first queen's position is important since it is the root point of our recursive tree. 
      * 
      * @param n
      * @return
@@ -54,7 +56,9 @@ public class NQueensII {
 		// iterate through placing the first queen in different spots in the first row
 		for(int firstQueenPos = 0; firstQueenPos<n; firstQueenPos++)
 		{
-			backtrackQueen(0,firstQueenPos,board); 
+			board[0][firstQueenPos] = true; // mark the location of the first queen
+			backtrackQueen(1,firstQueenPos,board); 
+			board[0][firstQueenPos] = false; //unmark the location for next iteration
 		}
 		
 		// Solutions will be accumulated by the recursive function adding to the total
@@ -63,7 +67,34 @@ public class NQueensII {
 	
 	public static boolean backtrackQueen(int row, int col, boolean[][] board)
 	{
+		
+		for(int column = 0; column<board[row].length; column++)
+		{
+			
+			if(isQueenSafe(row,col,board)){
+				if(column == board[row].length-1){
+					
+					return false; //if we've reached last column in row return false
+				}
+			}
+		}
+	    board[row][col] = true; // set the queen 
 		return false; 
 	}
+
+	/**
+	 * Determine if a Queen placed at a given row, column is under attack. 
+	 * Pass in the board that has the queen positions. 
+	 * 
+	 * @param row - row of the queen position
+	 * @param col - column of the queen position
+	 * @param board  - current state of the board
+	 * @return - true | the queen is safe -- false | the queen is under attack
+	 */
+	private static boolean isQueenSafe(int row, int col, boolean[][] board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 
 }
