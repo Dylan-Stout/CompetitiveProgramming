6ef019682f42bea526fd91350acffbece7bc8c69
Dylan Stout <dylan.d.stout@gmail.com>
2018-07-29 16:05:10 -0600

    Add backtrack algorithm

--- src/com/leetcode/problemset/NQueensII.java
+++ src/com/leetcode/problemset/NQueensII.java
@@ -1,5 +1,7 @@
 package com.leetcode.problemset;
 
+import java.util.HashSet;
+
 /**
  * https://leetcode.com/problems/n-queens-ii/description/
  * 
@@ -31,9 +33,37 @@ package com.leetcode.problemset;
  */
 public class NQueensII {
 
-	public static int totalNQueens(int i) {
-		// TODO Auto-generated method stub
-		return null;
+    public static final int TOTAL_SOLUTIONS = 0; 
+    public static HashSet<Integer> attackedColumns = new HashSet<Integer>(); 
+    public static HashSet<Integer> attackedRows = new HashSet<Integer>(); 
+	public static boolean[][] board = null; 
+    
+    /**
+     * First call made in recursive backtracking algorithm. Sets up an NxN 2D boolean 
+     * array for keeping track of queen positions. Makes call to recursive backtrack algorithm
+     * to find queen combinations through brute force permutations. 
+     * 
+     * @param n
+     * @return
+     */
+	public static int totalNQueens(int n) {
+		
+		// instantiate the board with no queens present
+		board = new boolean[n][n]; 
+		
+		// iterate through placing the first queen in different spots in the first row
+		for(int firstQueenPos = 0; firstQueenPos<n; firstQueenPos++)
+		{
+			backtrackQueen(0,firstQueenPos,board); 
+		}
+		
+		// Solutions will be accumulated by the recursive function adding to the total
+		return TOTAL_SOLUTIONS;
+	}
+	
+	public static boolean backtrackQueen(int row, int col, boolean[][] board)
+	{
+		return false; 
 	}
 
 }
