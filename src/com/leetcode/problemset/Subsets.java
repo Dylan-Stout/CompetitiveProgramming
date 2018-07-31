674a185a42e6b86d9010e319baea15355112ac19
Dylan Stout <dylan.d.stout@gmail.com>
2018-07-31 14:54:54 -0600

    Header fields

--- src/com/leetcode/problemset/Subsets.java
+++ src/com/leetcode/problemset/Subsets.java
@@ -3,7 +3,33 @@ package com.leetcode.problemset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+/**
+ * 
+ * https://leetcode.com/problems/subsets/description/
+ * 
+ * 
+ * Given a set of distinct integers, nums, return all possible subsets (the power set).
 
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+ * @author dystout
+ *
+ */
 public class Subsets {
 	
 	protected List<List<Integer>> knownSubsets = new ArrayList<List<Integer>>(); 
