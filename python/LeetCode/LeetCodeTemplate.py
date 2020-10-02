fc188beb8b5550cba731a9c5529f764ceea1a89e
Dylan Stout <dylan.d.stout@gmail.com>
2020-10-02 13:16:25 -0700

    BST Balancing and template

---
+++ python/LeetCode/LeetCodeTemplate.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class Solution(object):
+    def function(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        """
+        Perform all solution setup work for different test cases
+        :return:
+        """
+
+
+    # def test_funtion(self):
+    #     self.assertEqual(actual, expected)
---
+++ python/LeetCode/Trees/BalanceBST.py
@@ -0,0 +1,33 @@
+"""
+Given a binary search tree, return a balanced binary search tree with the same node values.
+
+A binary search tree is balanced if and only if the depth of the two subtrees of every node never differ by more than 1.
+
+If there is more than one answer, return any of them.
+
+https://leetcode.com/problems/balance-a-binary-search-tree/
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import unittest
+
+
+class Solution(object):
+    def balanceBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        """
+        Perform all solution setup work for different test cases
+        :return:
+        """
